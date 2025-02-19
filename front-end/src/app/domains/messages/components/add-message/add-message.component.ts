d0da59f2498e0de7c55132c4d251507e8f946bca
Evan BOISSONNOT <evan@boissonnot.fr>
2025-02-19 17:42:34 +0100

    feat: send a message and display in console received messages

--- front-end/src/app/domains/messages/components/add-message/add-message.component.ts
+++ front-end/src/app/domains/messages/components/add-message/add-message.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { defaultMessage, NewMessage } from '../../models';
+
+import *  as signalR from '@microsoft/signalr';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -11,10 +13,25 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   templateUrl: './add-message.component.html',
   styleUrl: './add-message.component.css'
 })
-export class AddMessageComponent {
+export class AddMessageComponent implements OnInit {
+  private readonly connection = new signalR.HubConnectionBuilder()
+                                .withUrl('https://localhost:49155/chathub')
+                                .build();
   message = signal(defaultMessage)
+
+  ngOnInit(): void {
+    this.connection.start()
+                   .then(() => console.info('HUB IS CONNECTED'))
+                   .catch((err) => console.error(err));
+
+    this.connection.on('messageReceived', (author, message) => {
+      console.info('message reçu ', { message, author })
+    })
+  }
 
   send(): void {
     console.info(this.message().content)
+    this.connection.send("newMessage", 'evan', this.message().content)
+                  .then(() => this.message().content = '');
   }
 }
