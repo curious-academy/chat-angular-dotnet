8e70ace606f26ed22cca24acbcd08d2cc5ff0d17
Evan BOISSONNOT <evan@boissonnot.fr>
2025-02-19 17:42:13 +0100

    chore: prepare web api feat: add ability to create a hub to send message

---
+++ ChatMiaou/back-end/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace back_end.Hubs
+{
+    public class ChatHub : Hub
+    {
+        #region Public methods
+        public async Task NewMessage(string user, string message)
+        {
+            await this.Clients.All.SendAsync("messageReceived", user, message);
+        }
+        #endregion
+    }
+}
---
+++ ChatMiaou/back-end/Program.cs
@@ -0,0 +1,66 @@
+using back_end.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var devCorsPolicy = "_devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR(options =>
+{
+
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors(devCorsPolicy);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+}
